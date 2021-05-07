5b54e3893bb91124d419499dd85d9dc04045edb8
Cristopher Oyarzún <c.oyarzun@gmail.com>
2021-05-06 23:47:44 -0400

    tests(android-lint): improve readability in the tests and add a new one

--- android-lint-rules/src/test/java/dev/cristopher/lint/rules/HardcodedColorXmlDetectorTest.kt
+++ android-lint-rules/src/test/java/dev/cristopher/lint/rules/HardcodedColorXmlDetectorTest.kt
@@ -2,51 +2,62 @@ package dev.cristopher.lint.rules
 
 import com.android.tools.lint.checks.infrastructure.TestFiles.xml
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.android.tools.lint.detector.api.Severity
 import org.junit.Test
 
 class HardcodedColorXmlDetectorTest {
 
-    @Test fun appCustomColor() {
+    @Test
+    fun `Given a hardcoded color on a custom text view property, When we analyze our custom rule, Then display an error`() {
         lint()
             .files(
                 xml(
                     "res/layout/layout.xml",
                     """
-          <TextView xmlns:app="http://schemas.android.com/apk/res-auto"
-              app:someCustomColor="#fff"/>"""
+                    <TextView xmlns:app="http://schemas.android.com/apk/res-auto"
+                    app:someCustomColor="#fff"/>
+                    """
                 ).indented()
             )
             .issues(HardcodedColorXmlDetector.ISSUE)
             .allowMissingSdk()
             .run()
-            .expect(
-                """
-          |res/layout/layout.xml:2: Error: Hardcoded hex colors should be declared in a '<color>' resource. [HardcodedColorXml]
-          |    app:someCustomColor="#fff"/>
-          |                         ~~~~
-          |1 errors, 0 warnings""".trimMargin()
-            )
+            .expectCount(1, Severity.ERROR)
     }
 
-    @Test fun textColor() {
+    @Test
+    fun `Given a hardcoded color on a text view, When we analyze our custom rule, Then display an error`() {
         lint()
             .files(
                 xml(
                     "res/layout/layout.xml",
                     """
                         <TextView xmlns:android="http://schemas.android.com/apk/res/android"
-                            android:textColor="#fff"/>"""
+                            android:textColor="#80000000"/>
+                        """
                 ).indented()
             )
             .issues(HardcodedColorXmlDetector.ISSUE)
             .allowMissingSdk()
             .run()
-            .expect(
-                """
-          |res/layout/layout.xml:2: Error: Hardcoded hex colors should be declared in a '<color>' resource. [HardcodedColorXml]
-          |    android:textColor="#fff"/>
-          |                       ~~~~
-          |1 errors, 0 warnings""".trimMargin()
+            .expectCount(1, Severity.ERROR)
+    }
+
+    @Test
+    fun `Given a color from our resources on a text view, When we analyze our custom rule, Then expect no errors`() {
+        lint()
+            .files(
+                xml(
+                    "res/layout/layout.xml",
+                    """
+                        <TextView xmlns:android="http://schemas.android.com/apk/res/android"
+                            android:textColor="@color/primaryColor"/>
+                        """
+                ).indented()
             )
+            .issues(HardcodedColorXmlDetector.ISSUE)
+            .allowMissingSdk()
+            .run()
+            .expectClean()
     }
 }
